a6fd9ef8798c2212eb08566c6953e4865daa557b
seonghyeoklee <dltjdgur327@gmail.com>
2024-09-29 20:16:42 +0900

    feature: fixed json abstract types issue

--- src/main/kotlin/com/evawovamynewsservice/controller/GoogleFormController.kt
+++ src/main/kotlin/com/evawovamynewsservice/controller/GoogleFormController.kt
@@ -1,6 +1,8 @@
 package com.evawovamynewsservice.controller
 
 import com.evawovamynewsservice.utils.logger
+import com.fasterxml.jackson.annotation.JsonSubTypes
+import com.fasterxml.jackson.annotation.JsonTypeInfo
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -32,6 +34,16 @@ data class FormResult(
     val response: Response,
 )
 
+@JsonTypeInfo(
+    use = JsonTypeInfo.Id.NAME,
+    include = JsonTypeInfo.As.PROPERTY,
+    property = "type", // JSON에 추가로 타입 정보를 포함할 필드
+)
+@JsonSubTypes(
+    JsonSubTypes.Type(value = Response.TextResponse::class, name = "TEXT"),
+    JsonSubTypes.Type(value = Response.CheckboxResponse::class, name = "CHECKBOX"),
+    JsonSubTypes.Type(value = Response.EmptyResponse::class, name = "EMPTY"),
+)
 sealed class Response {
     data class TextResponse(
         val response: String,
